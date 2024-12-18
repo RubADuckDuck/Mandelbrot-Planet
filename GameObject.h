755bee2f33d9533dbf49ea46fd024b226789bc3a
RubADuckDuck <kmo73724@gmail.com>
2024-12-18 18:59:55 +0900

    Added TerrainObject

--- GameObject.h
+++ GameObject.h
@@ -36,6 +36,65 @@ public:
 	virtual void onEvent(const std::string& message);
 }; 
 
+class TerrainObject : public GameObject {
+public:
+    static const int GRID_SIZE = 100;
+    float BLOCK_SIZE = 1.0f;
+    float BLOCK_OFFSET = 1.0f;
+
+    enum MapLayerType {
+        BIOME,        // Water, Grassland, volcano ...
+        BUILDING,
+        DROPPEDITEM,  // Resources
+        PLAYER,
+    };
+
+    enum GroundType {
+        WATER,
+        GRASS,
+    };
+
+    GroundType groundGrid[GRID_SIZE][GRID_SIZE];
+    Transform index2GroundTransform[GRID_SIZE][GRID_SIZE];
+
+    std::map<GroundType, GeneralMesh*> groundType2Mesh;    // Use pointers to GeneralMesh
+    std::map<GroundType, Texture*> groundType2Texture;     // Use pointers to Texture
+
+    void Update() override {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                // Set the transform positions for each block
+                float xCoord = j * BLOCK_OFFSET;
+                float yCoord = i * BLOCK_OFFSET;
+                index2GroundTransform[i][j].SetTranslation(glm::vec3(xCoord, 0.0f, yCoord));
+            }
+        }
+    }
+
+    void DrawGameObject(CameraObject& cameraObj) override {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                DrawBlockOfTerrainAt(i, j, cameraObj);
+            }
+        }
+    }
+
+    void DrawBlockOfTerrainAt(int yIndex, int xIndex, CameraObject& cameraObj) {
+        // Retrieve transform, ground type, mesh, and texture
+        Transform& currTransform = index2GroundTransform[yIndex][xIndex];
+        glm::mat4 transformMat = currTransform.GetTransformMatrix();
+
+        GroundType currGroundType = groundGrid[yIndex][xIndex];
+        GeneralMesh* currMesh = groundType2Mesh[currGroundType];
+        Texture* currTexture = groundType2Texture[currGroundType];
+
+        // Ensure mesh and texture are valid before rendering
+        if (currMesh && currTexture) {
+            currMesh->Render(cameraObj, transformMat, currTexture);
+        }
+    }
+};
+
 class RotatingGameObject : public GameObject {
 public: 
 	float radian = 0; 
